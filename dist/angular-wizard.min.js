bea7f0512818bf7d9b29294f972fdf5c2ae1f6a8
Martin Gontovnikas <martin@gon.to>
2014-02-13 16:50:10 -0300

    0.2.2

--- dist/angular-wizard.min.js
+++ dist/angular-wizard.min.js
@@ -1,6 +1,6 @@
 /**
  * Easy to use Wizard library for AngularJS
- * @version v0.2.1 - 2014-02-13 * @link https://github.com/mgonto/angular-wizard
+ * @version v0.2.2 - 2014-02-13 * @link https://github.com/mgonto/angular-wizard
  * @author Martin Gontovnikas <martin@gon.to>
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
