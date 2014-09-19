af8debb889d3765c7e2d981515d50215f3e98b39
Harry Moreno <morenoh149@gmail.com>
2014-09-19 04:05:07 -0700

    made navigation relative to the wizard. I think this is better than just placing the navbar at the bottom of the window. Also updated dev-dependencies

--- dist/angular-wizard.js
+++ dist/angular-wizard.js
@@ -1,6 +1,6 @@
 /**
  * Easy to use Wizard library for AngularJS
- * @version v0.4.0 - 2014-07-10 * @link https://github.com/mgonto/angular-wizard
+ * @version v0.4.0 - 2014-09-19 * @link https://github.com/mgonto/angular-wizard
  * @author Martin Gontovnikas <martin@gon.to>
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
