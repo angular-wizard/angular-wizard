553dca8825eaa3e0127654fcee39887a1bc8a316
jacobscarter <jacob@ieksolutions.com>
2015-06-11 08:24:55 -0700

    project build

--- dist/angular-wizard.js
+++ dist/angular-wizard.js
@@ -1,6 +1,6 @@
 /**
  * Easy to use Wizard library for AngularJS
- * @version v0.5.2 - 2015-06-08 * @link https://github.com/mgonto/angular-wizard
+ * @version v0.5.3 - 2015-06-11 * @link https://github.com/mgonto/angular-wizard
  * @author Martin Gontovnikas <martin@gon.to>
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
