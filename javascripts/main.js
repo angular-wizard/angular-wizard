e1010baa5a765dac62c72e3fe1eb60bb0e4d4ac6
Jacob Carter <jacob@ieksolutions.com>
2016-09-07 13:49:28 -0700

    update demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,7 +11,7 @@ angular.module('wizard-sample', ['mgo-angular-wizard'])
             console.log("Step continued");
         };
         $scope.goBack = function() {
-            WizardHandler.wizard().goTo(0);
+            WizardHandler.wizard('topWizard').goTo(1);
         };
         $scope.exitWithAPromise = function() {
             var d = $q.defer();
@@ -28,5 +28,30 @@ angular.module('wizard-sample', ['mgo-angular-wizard'])
         }
         $scope.exitValidation = function() {
             return $scope.canExit;
+        };
+
+        /* Logix for ngRepeat Wizard */
+        $scope.wizardSteps = [{
+            title: 'Step 1',
+            content: 'This is content for step 1'
+        }, {
+            title: 'Step 2',
+            content: 'This is content for step 2'
+        }];
+
+        $scope.stepToAdd = {
+            title: '',
+            content: ''
+        };
+
+        $scope.addStep = function() {
+            $scope.wizardSteps.push(angular.copy($scope.stepToAdd));
+            console.log('pushed: ', $scope.wizardSteps, $scope.stepToAdd);
+            $scope.stepToAdd.title = '';
+            $scope.stepToAdd.content = '';
+        };
+
+        $scope.removeStep = function(index) {
+            $scope.wizardSteps.splice(index, 1);
         };
     });
