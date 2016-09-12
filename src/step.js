5567b516703a199aa815f12172e09d266ae78bba
Ruben B <ruben_biesemans@hotmail.com>
2016-09-12 11:41:10 +0200

    Add translation support

--- src/step.js
+++ src/step.js
@@ -15,7 +15,14 @@ angular.module('mgo-angular-wizard').directive('wzStep', function() {
         templateUrl: function(element, attributes) {
           return attributes.template || "step.html";
         },
+        link: function ($scope, $element, $attrs, wizard) {
+            //OPTION1: ADD observe on the wzTitle attribute and change the title accordingly
+            $attrs.$observe('wzTitle', function (value) {
+                $scope.title = $scope.wzTitle;
+            });
             $scope.title = $scope.wzTitle;
+            //OPTION2:  DON'T set the title on the scope
+            //          So remove $scope.title = $scope.wzTitle;
             wizard.addStep($scope);
             $scope.$on('$destroy', function(){
                 wizard.removeStep($scope);
